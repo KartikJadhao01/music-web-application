dca439fc53c06a7f63a74c57f9f17bbcebaaa14b
KartikJadhao01 <143504998+KartikJadhao01@users.noreply.github.com>
2026-01-05 20:28:43 +0530

    Add files via upload

---
+++ htmlwebsite/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import redirect, render
+from django.http import JsonResponse
+
+
+
+
+def home(request):
+    
+    return render(request,'index.html',{
+        
+    })
+
+
+
